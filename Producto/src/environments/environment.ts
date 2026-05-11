84613093b9b00a09c7ef332e7a223aaba2003a24
Al0naas <alo.robles@duocuc.cl>
2026-05-11 06:56:47 -0400

    Add files via upload

---
+++ Producto/src/environments/environment.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: false,
+  supabaseUrl: 'https://ahtuiqrnrrqaglbsodig.supabase.co',
+  supabaseKey: 'sb_publishable_TjahixT1ESxG8BSShr7Vbw_Ug-9nEMZ',
+  geminiApiKey: 'AIzaSyCmOAA0hrQK1BAjJqooj1-Fq2TVM10p-zQ',
+};
